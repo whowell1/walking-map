6a4a62dea959cd8356c67a750ec984935bf9b6b8
Wren <wentarohowell@gmail.com>
2019-12-04 16:38:22 -0500

    workign Branch

--- src/App.js
+++ src/App.js
@@ -1,28 +1,85 @@
 import React from 'react';
 import logo from './logo.svg';
-import Map from './components/Map.js'
+// import Map from './components/Map.js'
 import InputBox from './components/InputBox.js'
 import './App.css';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+
 // import FormControl from 'react-bootstrap/lib/FormControl';
 // import {FormControl, FormGroup, ControlLabel, HelpBlock, Checkbox, Radio, Button} from 'react-bootstrap';
 
 import {Form, FormGroup, ControlLabel, FormControl,Button} from 'react-bootstrap'
-function App() {
+import {GoogleMap,withScriptjs,withGoogleMap,Circle} from "react-google-maps";
+const { MarkerWithLabel } = require("react-google-maps/lib/components/addons/MarkerWithLabel");
 
 
 
+function Map() {
   return (
+  <GoogleMap 
+  defaultZoom = {10}  
+  defaultCenter={{ lat: 33.7490, lng: 150.644 }}
+    
+ >
+ <Circle
+    radius ={100}
+    visible = {true}
+    defaultCenter={{ lat: -34.397, lng: 150.644 }}
+	
+      // position={{ lat: -34.397, lng: 150.644 }}
+      // labelStyle={{backgroundColor: "yellow", fontSize: "32px", padding: "16px"}}
+    >
+      {/* <div>Hello There!</div> */}
+    </Circle> </GoogleMap>
+ 
+  );
+}
 
-    <div className="App">
-      <header className="App-header">
-      <Map/>
-      <InputBox/>
+const WrappedCircle = withScriptjs(withGoogleMap(Map));
+
+
+export default function App() {
+  return (
+    <div className="App-header">
+    <div style={{ width: "100vw", height: "100vh" }}>
+      <WrappedCircle
+      googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyAkcLnJRWmnplhaoKJZJj4lbT1iNNvkxos&v=3.exp&libraries=geometry,drawing,places"       
+       loadingElement={<div style={{ height: `100%` }} />}
+        containerElement={<div style={{ height: `100%` }} />}
+        mapElement={<div style={{ height: `100%` }} />}
+      />
+    </div>
+    </div>
+  );
+}
+
+
+
+
+
+
+
+
+/*** */
+// function App() {
+
+  // return (
+
+    // <div className="App">
+    //   <header className="App-header">
+    //   <div style= {{width: '100vw', veight:'100vh'}} >
+    //   <WrappedCircle GoogleMapURL= {"https://maps.googleapis.com/maps/api/js?key=AIzaSyAkcLnJRWmnplhaoKJZJj4lbT1iNNvkxos&v=3.exp&libraries=geometry,drawing,places"}
+    //   loadingElement={<div style={{height:"100%"}} />}
+    //    containerElement= {<div style={{ height: `400px` }} />}
+    //     mapElement = {<div style={{ height: `100%` }} />}
+    //   />
+    //   </div>
+    //   <InputBox/>
 
     
         
-      {/* <Form>
+      /* { <Form>
   <Form.Group controlId="formBasicEmail">
     <Form.Control type="email" placeholder="Enter number of steps" />
     <Form.Text className="text-muted">
@@ -31,13 +88,13 @@ function App() {
   <Button variant="primary" type="submit">
     Submit
   </Button>
-</Form> */}
+</Form> */
 
       
     
-      </header>
-    </div>
-  );
-}
+    //   </header>
+    // </div>
+//   );
+// }
 
-export default App;
+// export default App;
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,82 +1,76 @@
 import React from "react"
-
-
-
-
-const fetch = require("isomorphic-fetch");
-const { compose, withProps, withHandlers } = require("recompose");
+const { compose, withProps, withStateHandlers } = require("recompose");
+const FaAnchor = require("react-icons/lib/fa/anchor");
 const {
   withScriptjs,
   withGoogleMap,
   GoogleMap,
   Marker,
-  Circle
+  InfoWindow,
 } = require("react-google-maps");
-const { MarkerClusterer } = require("react-google-maps/lib/components/addons/MarkerClusterer");
 
-const MapWithAMarkerClusterer = compose(
-  withProps({
-    googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyAkcLnJRWmnplhaoKJZJj4lbT1iNNvkxos&v=3.exp&libraries=geometry,drawing,places",
-    loadingElement: <div style={{ height: `1000px`, width: `1000px`}} />,
-    containerElement: <div style={{ height: `1000px`, width: `1000px` }} />,
-    mapElement: <div style={{ height: `100%` }} />,
-  }),
-  withHandlers({
-    onMarkerClustererClick: () => (markerClusterer) => {
-      const clickedMarkers = markerClusterer.getMarkers()
-      console.log(`Current clicked markers length: ${clickedMarkers.length}`)
-      console.log(clickedMarkers)
-    },
+const MapWithAMakredInfoWindow = compose(
+  withStateHandlers(() => ({
+    isOpen: false,
+  }), {
+    onToggleOpen: ({ isOpen }) => () => ({
+      isOpen: !isOpen,
+    })
   }),
   withScriptjs,
   withGoogleMap
 )(props =>
   <GoogleMap
-    defaultZoom={9}
-    defaultCenter={{ lat: 33.7490, lng: -84.3880 }}
+    defaultZoom={8}
+    defaultCenter={{ lat: -34.397, lng: 150.644 }}
   >
-    <MarkerClusterer
-      onClick={props.onMarkerClustererClick}
-      averageCenter
-      enableRetinaIcons
-      gridSize={60}
+    <Marker
+      position={{ lat: -34.397, lng: 150.644 }}
+      onClick={props.onToggleOpen}
     >
-      {props.markers.map(marker => (
-        <Marker
-          key={marker.photo_id}
-          position={{ lat: marker.latitude, lng: marker.longitude }}
-        />
-      ))}
-    </MarkerClusterer>
+      {props.isOpen && <InfoWindow onCloseClick={props.onToggleOpen}>
+        <FaAnchor />
+      </InfoWindow>}
+    </Marker>
   </GoogleMap>
 );
 
-class Map extends React.PureComponent {
-  componentWillMount() {
-    this.setState({ markers: [] })
-  }
+<MapWithAMakredInfoWindow
+  googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyAkcLnJRWmnplhaoKJZJj4lbT1iNNvkxos&v=3.exp&libraries=geometry,drawing,places"
+  loadingElement={<div style={{ height: `100%` }} />}
+  containerElement={<div style={{ height: `400px` }} />}
+  mapElement={<div style={{ height: `100%` }} />}
+/>
 
-  componentDidMount() {
-    const url = [
-      // Length issue
-      `https://gist.githubusercontent.com`,
-      `/farrrr/dfda7dd7fccfec5474d3`,
-      `/raw/758852bbc1979f6c4522ab4e92d1c92cba8fb0dc/data.json`
-    ].join("")
 
-    fetch(url)
-      .then(res => res.json())
-      .then(data => {
-        this.setState({ markers: data.photos });
-      });
-  }
+// class Map extends React.PureComponent {
+//   componentWillMount() {
+//     this.setState({ markers: [] })
+//   }
 
-  render() {
-    return (
-      <MapWithAMarkerClusterer markers={this.state.markers} />
-    )
-  }
-}
+//   componentDidMount() {
+//     const url = [
+//       // Length issue
+//       `https://gist.githubusercontent.com`,
+//       `/farrrr/dfda7dd7fccfec5474d3`,
+//       `/raw/758852bbc1979f6c4522ab4e92d1c92cba8fb0dc/data.json`
+//     ].join("")
+
+//     fetch(url)
+//       .then(res => res.json())
+//       .then(data => {
+//         this.setState({ markers: data.photos });
+//       });
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <p> hi</p>
+//       </div>
+//     )
+//   }
+// }
 
 
 
