91987850fac7dfde9cd6ccdc9c2910ac65d48699
Wren <wentarohowell@gmail.com>
2019-12-04 20:00:11 -0500

    testing with updated api

--- src/App.js
+++ src/App.js
@@ -4,91 +4,57 @@ import logo from './logo.svg';
 // import Map from './components/Map.js'
 import InputBox from './components/InputBox.js'
 import './App.css';
+import { Circle, GoogleMap } from '@react-google-maps/api';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
+const { LoadScript } = require("./LoadScript");
+const ScriptLoaded = require("./docs/ScriptLoaded").default;
 
-// import FormControl from 'react-bootstrap/lib/FormControl';
-// import {FormControl, FormGroup, ControlLabel, HelpBlock, Checkbox, Radio, Button} from 'react-bootstrap';
-
-import {Form, FormGroup, ControlLabel, FormControl,Button} from 'react-bootstrap';
-
-
-const { compose, withProps } = require("recompose");
-
-const google=window.google;
-
-const {
-  withScriptjs,
-  withGoogleMap,
-  GoogleMap,
-  StreetViewPanorama,
-  OverlayView
-} = require("react-google-maps");
-
-const { DrawingManager } = require("react-google-maps/lib/components/drawing/DrawingManager");
-
-// const { StreetViewPanormaWithAnOverlayView } = require("react-google-maps/lib/components/drawing/StreetViewPanormaWithAnOverlayView");
-
-// const getPixelPositionOffset = (width, height) => ({
-//   x: -(width / 2),
-//   y: -(height / 2),
-// })
-
-
-function Map() {
-  return (
-  <GoogleMap 
-  defaultZoom = {10}  
-  defaultCenter={{ lat: 33.7490, lng: -84.3880 }}
-
-
-  
-  
-    
- >
-  <DrawingManager
-      defaultDrawingMode={google.maps.drawing.OverlayType.CIRCLE}
-      defaultOptions={{
-        drawingControl: true,
-        drawingControlOptions: {
-          position: google.maps.ControlPosition.TOP_CENTER,
-          drawingModes: [
-            google.maps.drawing.OverlayType.CIRCLE,
-            google.maps.drawing.OverlayType.POLYGON,
-            google.maps.drawing.OverlayType.POLYLINE,
-            google.maps.drawing.OverlayType.RECTANGLE,
-          ],
-        },
-        circleOptions: {
-          fillColor: `#ffff00`,
-          fillOpacity: 1,
-          strokeWeight: 5,
-          clickable: false,
-          editable: true,
-          zIndex: 1,
-        },
-      }}
-    />
-    </GoogleMap>
- 
-  );
+const mapContainerStyle = {
+  height: "400px",
+  width: "800px"
 }
 
-const WrappedCircle = withScriptjs(withGoogleMap(Map));
+const center = {
+  lat: -3.745,
+  lng: -38.523
+}
+
+const options = {
+  strokeColor: '#FF0000',
+  strokeOpacity: 0.8,
+  strokeWeight: 2,
+  fillColor: '#FF0000',
+  fillOpacity: 0.35,
+  clickable: false,
+  draggable: false,
+  editable: false,
+  visible: true,
+  radius: 30000,
+  zIndex: 1
+}
+
+const onLoad = circle => {
+  console.log('Circle onLoad circle: ', circle)
+}
+
+const onUnmount = circle => {
+  console.log('Circle onUnmount circle: ', circle)
+}
+
+
 
 
 export default function App() {
   return (
-    <div className="App-header">
-    <div style={{ width: "100vw", height: "100vh" }}>
-      <WrappedCircle
-      googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyAkcLnJRWmnplhaoKJZJj4lbT1iNNvkxos&v=3.exp&libraries=geometry,drawing,places"       
-       loadingElement={<div style={{ height: `100%` }} />}
-        containerElement={<div style={{ height: `100%` }} />}
-        mapElement={<div style={{ height: `100%` }} />}
-      />
-    </div>
-    </div>
+    <LoadScript
+        id="script-loader"
+        googleMapsApiKey="AIzaSyAkcLnJRWmnplhaoKJZJj4lbT1iNNvkxos&v=3.exp&libraries=geometry,drawing,places"
+      >
+        <GoogleMap
+          id='example-map'
+        >
+        </GoogleMap>
+      </LoadScript>
   );
 }
 
